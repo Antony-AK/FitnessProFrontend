d65d633df872b6aca7dd8d7075ca4af0666ef79d
Antony-AK <antonyak147@gmail.com>
2026-03-15 10:13:28 +0530

    api updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-// export const FITNESS_API = "https://fitnesspro-ch1m.onrender.com" 
+export const FITNESS_API = "https://fitnesspro-ch1m.onrender.com" 
 
-export const FITNESS_API = "http://localhost:5000"
+// export const FITNESS_API = "http://localhost:5000"
 
